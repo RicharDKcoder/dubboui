579b3383a3b57214fb148a52183176ea6d22a79e
docryze <docryze@163.com>
2020-07-08 01:56:14 +0800

    add test rest

--- dubboui-service/src/main/java/org/docryze/dubboui/service/impl/DubboUiServiceImpl.java
+++ dubboui-service/src/main/java/org/docryze/dubboui/service/impl/DubboUiServiceImpl.java
@@ -4,7 +4,9 @@ import com.google.common.collect.Lists;
 import org.apache.commons.lang.StringUtils;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.dubbo.config.ApplicationConfig;
+import org.apache.dubbo.config.ReferenceConfig;
 import org.apache.dubbo.config.RegistryConfig;
+import org.apache.dubbo.rpc.model.ServiceMetadata;
 import org.apache.zookeeper.data.Stat;
 import org.docryze.dubboui.service.DubboUiService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,6 +43,14 @@ public class DubboUiServiceImpl implements DubboUiService {
 
         Map<String, String> parameters = registryConfig.getParameters();
         Map<String, String> metaData = registryConfig.getMetaData();
+
+        ReferenceConfig reference = new ReferenceConfig(); // 此实例很重，封装了与注册中心的连接以及与提供者的连接，请自行缓存，否则可能造成内存和连接泄漏
+        reference.setRegistry(registryConfig); // 多个注册中心可以用setRegistries()
+        reference.setVersion("1.0.0");
+
+        // 和本地bean一样使用xxxService
+        ServiceMetadata serviceMetadata = reference.getServiceMetadata();
+
 
         if(StringUtils.isBlank(className)){
             return Lists.newArrayList();
